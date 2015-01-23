933851531ef8f312080e64e7404085e2702b846a
lungau4ever <dorfer.manuel@gmail.coom>
2015-01-23 17:42:20 +0100

    Updated version including layouts

--- app/src/main/java/com/example/floriandodegge/spinsecat/MainActivity.java
+++ app/src/main/java/com/example/floriandodegge/spinsecat/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start_activity);
 
         img = (ImageView) findViewById(R.id.image);
 
@@ -89,7 +89,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.start_activitiy, menu);
         return true;
     }
 
