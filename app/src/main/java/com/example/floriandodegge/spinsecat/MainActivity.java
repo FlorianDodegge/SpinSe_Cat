e800b9876888a19413a66bdad48711098878c74c
Bernhard1991 <bernhard.mandl1991@gmail.com>
2015-01-28 17:58:06 +0100

    test

--- app/src/main/java/com/example/floriandodegge/spinsecat/MainActivity.java
+++ app/src/main/java/com/example/floriandodegge/spinsecat/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.floriandodegge.spinsecat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,9 +9,11 @@ import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 
@@ -18,16 +21,17 @@ public class MainActivity extends Activity {
 
     private ImageView img;
     private GestureDetector gd;
-    private float xDown, xUp, yDown, yUp, deltaX, deltaY, rotation;
+    private float xDown, xUp, yDown, yUp, deltaX, deltaY;
     private Animation rotationRight, rotationLeft;
     private int minSwipeDelta;        //min Distanz
+    private int randomID;       //Randomzahl für Datenbank ID      //Todo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_activity);
 
-        img = (ImageView) findViewById(R.id.image);
+        img = (ImageView) findViewById(R.id.imageView);
 
         rotationRight = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right);
         rotationLeft = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.left);
@@ -65,9 +69,13 @@ public class MainActivity extends Activity {
                             Log.i("SWIPE", "right");
                             img.startAnimation(rotationRight);
 
+                            randomID();
+
                         } else {
                             Log.i("SWIPE", "left");
                             img.startAnimation(rotationLeft);
+
+                            randomID();
                         }
                     }
                     result = true;
@@ -75,15 +83,27 @@ public class MainActivity extends Activity {
                     if (deltaY < 0) {
                         Log.i("SWIPE", "left");
                         img.startAnimation(rotationLeft);
+
+                        randomID();
                     } else {
                         Log.i("SWIPE", "right");
                         img.startAnimation(rotationRight);
+
+                        randomID();
                     }
                 }
 
                 break;
         }
         return true;
+    }
+
+    public int randomID(){
+        randomID = (int) ((Math.random() * 10))+1;
+
+        Log.i("RANDOM", "randomID = " + randomID);
+
+        return randomID;
     }
 
     @Override
@@ -105,5 +125,3 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
-
-//Test
